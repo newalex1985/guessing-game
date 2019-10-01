2a68e18e24f531ce86d276c5e58b9f81dfc552fd
Aliaksandr Konan <newalex1985@tut.by>
2019-10-02 00:01:07 +0300

    feat: add solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = {
+            min: 0,
+            max: 0
+        }
+        this.result;
+    }
 
     setRange(min, max) {
-
+        this.range.min = min;
+        this.range.max = max;
     }
 
     guess() {
-
+        const { min, max } = this.range; 
+        this.result = Math.ceil((min + max) / 2);
+        return this.result; 
     }
 
     lower() {
-
+        this.range.max = this.result;
     }
 
     greater() {
-
-    }
+        this.range.min = this.result;
+    }   
 }
 
 module.exports = GuessingGame;
